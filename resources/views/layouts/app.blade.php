b6a96de5342ded3fb5519861d73287b7582e23fd
Madhukar Shedthikere Prabhakara <msprabhakara@gmail.com>
2019-06-01 21:24:20 -0700

    with identity api

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,10 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script type="text/javascript">
+        var email='msprabhakara@gmail.com';
     window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
       heap.load("2990430872");
+      heap.identify(email);
 </script>
 </head>
 <body>
