5ba8b8c1819b204e84b6c1ea92f758ac83782556
Madhukar Shedthikere Prabhakara <msprabhakara@gmail.com>
2019-06-01 22:21:37 -0700

    id

--- resources/views/ulytics_views/dashboardv1.blade.php
+++ resources/views/ulytics_views/dashboardv1.blade.php
@@ -10,7 +10,7 @@
 	<meta content="" name="description" />
 	<meta content="" name="author" />
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<meta name="email-address" content="{{ \Auth::user()->email }}">
+	<meta name="email-address" content="{{ \Auth::id() }}">
 	<!-- ================== BEGIN BASE CSS STYLE ================== -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 	<link href="../assets/plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="email-address" content="{{ \Auth::user()->email }}">
+        <meta name="email-address" content="{{ \Auth::id() }}">
         <title>ulytics</title>
 
         <!-- Fonts -->
